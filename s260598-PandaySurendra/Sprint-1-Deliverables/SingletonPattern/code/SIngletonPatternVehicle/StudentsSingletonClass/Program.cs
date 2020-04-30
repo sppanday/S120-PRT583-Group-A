49094692fad8145e63568fcb0fc964b824f9f4e9
panday.sp@gmail.com <surendrapanday@SURENDRAs-iMac.local>
2020-05-01 08:54:17 +1000

    added singleton pattern - documentation for explanation/code -uml diagram -code snippet

---
+++ s260598-PandaySurendra/Sprint-1-Deliverables/SingletonPattern/code/SIngletonPatternVehicle/StudentsSingletonClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StudentsSingleton
+{
+    /**
+     * @author : Surendra Panday (s260598)
+     * PRT583 Project Development Methodology    
+     */
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Students engineer = Students.getInstance();
+
+            Students architect = Students.getInstance();
+
+            Students plumber = Students.getInstance();
+
+            Students mathematician = Students.getInstance();
+
+            Students mechanic = Students.getInstance();
+        }
+    }
+    class Students
+    {
+        private static Students uniqueStudents;
+
+        private Students() { }
+
+        public static Students getInstance()
+        {
+            if (uniqueStudents == null)
+            {
+                uniqueStudents = new Students();
+            }
+            return uniqueStudents;
+        }
+    }
+}
