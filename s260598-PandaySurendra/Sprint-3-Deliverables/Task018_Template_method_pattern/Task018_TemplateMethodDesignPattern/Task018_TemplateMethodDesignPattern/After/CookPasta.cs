0e9602f03bea0854332dfa16255b6bad07c9cb08
panday.sp@gmail.com <surendrapanday@SURENDRAs-iMac.local>
2020-05-14 20:44:14 +1000

    added template method pattern

---
+++ s260598-PandaySurendra/Sprint-3-Deliverables/Task018_Template_method_pattern/Task018_TemplateMethodDesignPattern/Task018_TemplateMethodDesignPattern/After/CookPasta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Task018_TemplateMethodDesignPattern.After
+{
+    public class CookPasta: CookingRecipe
+    {
+        public CookPasta()
+        {
+        }
+
+        public void addSauce()
+        {
+            Console.WriteLine('Adding tomato sauce'); // only tomato sauce not barbecue sauce which is in noodle
+        }
+        public void addCondiments()
+        {
+            Console.WriteLine('Adding salt'); // only salt, not pepper
+        }
+    }
+}
