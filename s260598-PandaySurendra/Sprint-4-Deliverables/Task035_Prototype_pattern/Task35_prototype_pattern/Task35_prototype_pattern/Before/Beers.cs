92f7c17725ee8ca9b0cb3fe4367fac510ee09248
panday.sp@gmail.com <surendrapanday@SURENDRAs-iMac.local>
2020-05-17 19:29:56 +1000

    added prototype design pattern

---
+++ s260598-PandaySurendra/Sprint-4-Deliverables/Task035_Prototype_pattern/Task35_prototype_pattern/Task35_prototype_pattern/Before/Beers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task35_prototype_pattern.Before
+{
+    public class Beers
+    {
+        public string beerName { set; get; };
+        public string beerId { set; get; };
+        public double beerPrice { set; get; };
+        public void setBeerName(string beerName)
+        {
+            this.beerName = beerName;
+        }
+
+        internal void setBeerId(string beerId)
+        {
+            this.beerId = beerId;
+        }
+
+        public void setBeerPrice(double beerPrice)
+        {
+            this.beerPrice = beerPrice;
+        }
+    }
+}
